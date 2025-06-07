Code with broadcasts was very confusing (sleepiness). Must fix many bugs tomorrow. gn

src/autograd/Tensor.py
@@ -130,29 +130,30 @@ class Tensor:
     def __truediv__(self, other):
         if isinstance(other, Tensor):
             other_data = other.data
+            self_data = self.data
             if (self.data.shape != other.data.shape):
                 if (self.data.shape[0] == other.data.shape[0] and other.data.shape[1] == 1):
                     other.data = np.tile(other.data, (1, self.data.shape[1]))
                 elif (self.data.shape[1] == other.data.shape[1] and other.data.shape[0] == 1):
                     other_data = np.tile(other.data, (self.data.shape[0], 1))
+                elif (self.data.shape[0] == other.data.shape[0] and self.data.shape[1] == 1):
+                    self_data = np.tile(self.data, (1, other.data.shape[1]))
                 else:
                     raise ValueError("Shapes of tensors do not match for multiplication.")
         else:
             other = Tensor(np.full_like(self.data, other, dtype=np.float32))
             other_data = other.data
+            self_data = self.data
 
-        out = Tensor(self.data / other_data, _children={self, other})
+        out = Tensor(self_data / other_data, _children={self, other})
 
         def _backward():
-            self.grad += out.grad * (1/other_data)
-            other_grad = out.grad * (-self.data/ (other.data ** 2))
-            if (other.grad.shape != self.grad.shape):
-                if (other.grad.shape[0] == self.grad.shape[0] and self.grad.shape[1] == 1):
-                    other.grad += other_grad.sum(axis=1)
-                elif (other.grad.shape[1] == self.grad.shape[1] and other.grad.shape[0] == 1):
-                    other.grad += other_grad.sum(axis=0)
+            if (other.grad.shape != self.grad.shape and other.grad.shape[0] == self.grad.shape[0] and (self.grad.ndim == 1 or self.grad.shape[1] == 1)):
+                self.grad += (out.grad * (1/other_data)).sum(axis=1)
             else:
-                other.grad += other_grad        
+                self.grad += out.grad * (1/other_data)
+
+            other.grad += out.grad * (-self.data/ (other.data ** 2))     
         out._backward = _backward
 
         return out
@@ -199,5 +200,5 @@ class Tensor:
     def softmax(self):
         sExp = self.exp()
         s = sExp.sum(axis=1)
-        out = s / sExp
+        out = sExp / s
         return out

src/nn.py
@@ -1,22 +1,46 @@
 import numpy as np
 from autograd.Tensor import Tensor
 from autograd.Layer import *
+import pickle
 
+# Load data
+pickle_path = "data/iris/pickled/"
+with open(pickle_path + "X_train.pkl", "rb") as f:
+    X_train = pickle.load(f)
 
+with open(pickle_path + "Y_train.pkl", "rb") as f:
+    Y_train = pickle.load(f)
+
+with open(pickle_path + "X_test.pkl", "rb") as f:
+    X_test = pickle.load(f)
+
+with open(pickle_path + "Y_test.pkl", "rb") as f:
+    Y_test = pickle.load(f)
 
 
 def categorical_cross_entropy(pred, target):
     return -(((target * (pred + 1e-9).log()).sum(axis=1)).mean(axis=0))
 
 
-X = Tensor(np.random.rand(10, 15).astype(np.float32))
-nn = MLP(15, 3, nLayers=3, nhidden=[32, 16])
+nn = MLP(4, 3, nLayers=2, nhidden=[15])
 
-out = nn.forward(X)
-Y_true = Tensor(np.array([[1,0,0], [0,1,0], [0,0,1], [1,0,0], [0,1,0], [0,0,1], [1,0,0], [0,1,0], [0,0,1], [1,0,0]], dtype=np.float32))
-loss = categorical_cross_entropy(out, Y_true)
-loss.backward()
-print(loss)
-print("Gradients:")
-for param in nn.params:
-    print(f"g: {param.grad}")
+MINIBATCH_SIZE = 15
+LR = 0.001
+
+for i in range(100):
+    # build minibatch
+    ix = np.random.randint(0, X_train.shape[0], MINIBATCH_SIZE)
+    X_batch = Tensor(X_train[ix])
+    Y_batch = Tensor(Y_train[ix])
+
+    # forward pass
+    out = nn.forward(X_batch)
+    print(out)
+    loss = categorical_cross_entropy(out, Y_batch)
+    print(f"Batch nº{i+1}. Loss:", loss.data)
+    # backward pass
+    nn.zero_grad()
+    loss.backward()
+    # update parameters
+    for param in nn.params:
+        param.data -= LR * param.grad